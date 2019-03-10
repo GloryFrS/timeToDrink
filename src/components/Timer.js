667618e5ff46ba4d9db4233fe2b95530008a0e14
Maryzv <zavmasha1@gmail.com>
2019-03-10 22:35:50 +0700

    хз

---
+++ src/components/Timer.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {secondsToTime} from "../params/Params";
+
+class Timer extends React.Component {
+    _isMounted = false;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            seconds: 0
+        }
+    };
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        this.setNewState(nextProps.seconds);
+    };
+
+    componentDidMount() {
+        this._isMounted = true;
+        this.setNewState(this.props.seconds);
+    }
+
+    startTimer() {
+        if (this.timer) clearInterval(this.timer);
+        this.timer = setInterval(() => {
+            if (this._isMounted) this.setState({
+                seconds: this.state.seconds !== 0 ? this.state.seconds - 1 : this.state.seconds
+            })}, 1000);
+    };
+
+    setNewState(seconds) {
+        if (seconds) {
+            this.setState({seconds: seconds}, this.startTimer);
+        }
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+        clearInterval(this.timer);
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>{secondsToTime(this.state.seconds)}</h2>
+            </div>
+        );
+    };
+}
+
+
+export default Timer;
