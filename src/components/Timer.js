c42d5877845f93178000a8dff7cdb7ea0f34c9dc
Alexandr <alexandr_150@mail.ru>
2019-03-12 22:43:42 +0700

    added style for welldone popup

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -42,7 +42,7 @@ class Timer extends React.Component {
     render() {
         return (
             <div>
-                <h2>{secondsToTime(this.state.seconds)}</h2>
+                <h2 className="timer">{secondsToTime(this.state.seconds)}</h2>
             </div>
         );
     };
--- src/popups/WellDonePopup.js
+++ src/popups/WellDonePopup.js
@@ -7,9 +7,9 @@ class WellDonePopup extends React.Component {
     render() {
         return (
             <div className="well-done-popup-container" onClick={()=>this.props.changeWellDonePopupVisibility()}>
-                <h1>Прекрасно</h1>
-                <h2>Следующий прием жидкости через:</h2>
-                <Timer seconds={Params.TIME_BETWEEN_WATER_INTAKES}/>
+                <h1 className="well-done-popup-h1">Прекрасно!</h1>
+                <h2 className="well-done-popup-h2">Следующий прием жидкости через:</h2>
+                <Timer className="well-done-popup-timer" seconds={Params.TIME_BETWEEN_WATER_INTAKES}/>
             </div>
         )
     }
