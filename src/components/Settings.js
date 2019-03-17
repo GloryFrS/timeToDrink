69c1ab8cf5742273061aaf8a6c8ef34ff55fc31e
Maryzv <zavmasha1@gmail.com>
2019-03-17 23:57:49 +0700

    settings page almost done

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Link from "react-router-dom/es/Link";
-import BackIcon from "../img/back.png";
+import BackIcon from "../img/back.svg";
 import InfoIcon from "../img/info.png";
+import "./Settings.css";
 
 class Settings extends React.Component {
 
 
     getNewParameters = () => {
         const newParameters = {
-            weekdaysWakeUp: document.getElementById("weekdays-time-wake-up").value,
-            weekdaysGoTOSleep: document.getElementById("weekdays-time-go-to-sleep").value,
-            weekendsWakeUp: document.getElementById("weekends-time-wake-up").value,
-            weekendsGoTOSleep: document.getElementById("weekends-time-go-to-sleep").value,
-            weight: document.getElementById("weight").value,
+            weekdaysWakeUp: document.getElementById("settings-weekdays-time-wake-up").value,
+            weekdaysGoTOSleep: document.getElementById("settings-weekdays-time-go-to-sleep").value,
+            weekendsWakeUp: document.getElementById("settings-weekends-time-wake-up").value,
+            weekendsGoTOSleep: document.getElementById("settings-weekends-time-go-to-sleep").value,
+            weight: document.getElementById("settings-weight").value,
         };
 
         if (newParameters.weekdaysWakeUp === '' || newParameters.weekdaysGoTOSleep === '' || newParameters.weight === '' ||
@@ -33,32 +34,32 @@ class Settings extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="settings-body-container">
                 <Link to='/main'>
-                    <img src={BackIcon} width='25px' alt=''/>
+                    <img src={BackIcon} alt=''/>
                 </Link>
                 <Link to='/info'>
                     <img src={InfoIcon} width='25px' alt=''/>
                 </Link>
                 <br/>
-                <p>Укажите свой актуальный вес:</p>
-                <input type="number" min="1" max="250" id="weight" onChange={this.onChange}/>
+                <p className="settings-decree">Укажите свой актуальный вес:</p>
+                <input type="number" min="1" max="250"  className="settings-weight" id="weight" onChange={this.onChange}/>
                 <br/>
-                <div style={{display: 'inline'}}>
-                    <span>По будням я просыпаюсь в </span>
-                    <input type="time" id="weekdays-time-wake-up"/>
-                    <span> и засыпаю в </span>
-                    <input type="time" id="weekdays-time-go-to-sleep"/>
+                <div className='settings-container-info' style={{display: 'inline'}}>
+                    <p className='settings-wakeup-text'>В будние дни:</p>
+                    <span className='settings-wake-time'>Просыпаюсь в: &nbsp;&nbsp;&nbsp;&nbsp;  Засыпаю в:</span> <br/>
+                    <div className='settings-time-to-wake-sleep'><input type="time" id="settings-weekdays-time-wake-up" className='settings-time-to-wake-up'/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input type="time" id="settings-weekdays-time-go-to-sleep" className='settings-time-to-sleep'/></div>
                 </div>
                 <br/>
                 <div style={{display: 'inline'}}>
-                    <span>По будням я просыпаюсь в </span>
-                    <input type="time" id="weekends-time-wake-up"/>
-                    <span> и засыпаю в </span>
-                    <input type="time" id="weekends-time-go-to-sleep"/>
+                    <p className='settings-wakeup-text'>В выходные дни:</p>
+                    <span className='settings-wake-time'>Просыпаюсь в: &nbsp;&nbsp;&nbsp;&nbsp;  Засыпаю в:</span> <br/>
+                    <div className='settings-time-to-wake-sleep'><input type="time" id="settings-weekends-time-wake-up" className='settings-time-to-wake-up'/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input type="time" id="settings-weekends-time-go-to-sleep" className='settings-time-to-sleep'/></div>
                 </div>
                 <br/>
-                <Link to='/main' onClick={this.checkAndUpdateParameters} >Сохранить</Link>
+                <Link to='/main' className="settings-save-button" onClick={this.checkAndUpdateParameters} >Сохранить</Link>
             </div>
         )
     };
